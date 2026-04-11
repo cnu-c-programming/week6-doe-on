99c8f809d2bfbc3cef76b01ded7d103b24bbb717
doe-on <fovusshi01197@gmail.com>
2026-04-11 20:55:58 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int arr[6] = {1,2,3,4,5,6,};
+    int arr[6] = {1,2,3,4,5,6};
     int* p = arr;
     int** pp = &p;
 
     for (int i=0; i<6; i++) {
-        printf("%d ", **(pp + i));
+        printf("%d ", *(*pp + i));
     }
 }
